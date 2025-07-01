56a6b0124ca04da7f87287e1fb7e66a8d7927cdc
joaquimlcbfranco <joaquimlcbfranco@gmail.com>
2025-07-01 11:30:54 +0100

    feat: Create initial Person class structure

--- src/main/java/projects/librarymanagement/Person.java
+++ src/main/java/projects/librarymanagement/Person.java
@@ -1,4 +1,26 @@
 package projects.librarymanagement;
 
 public class Person {
+
+    private String name;
+    private int age;
+    private String username;
+    private String password;
+
+    public Person(String name, int age, String username, String password) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return this.name;
+    };
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void printDetails() {
+        System.out.println(this.name + ", " + this.age + " years old.");
+    }
 }

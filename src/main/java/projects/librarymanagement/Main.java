da2f668157b1aabbf001b06de5ae06f5d3f9d13d
joaquimlcbfranco <joaquimlcbfranco@gmail.com>
2025-07-02 08:33:28 +0100

    feat: Indent for better readability

--- src/main/java/projects/librarymanagement/Main.java
+++ src/main/java/projects/librarymanagement/Main.java
@@ -6,8 +6,15 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        UserInterface ui = new UserInterface(scanner);
 
+        BookList books = new BookList();
+        books.addBook("To Kill a Mockingbird", "Harper Lee", 1960);
+        books.addBook("1984", "George Orwell", 1949);
+        books.addBook("The Great Gatsby", "F. Scott Fitzgerald", 1925);
+        books.addBook("Pride and Prejudice", "Jane Austen", 1813);
+        books.addBook("The Catcher in the Rye", "J.D. Salinger", 1951);
+
+        UserInterface ui = new UserInterface(scanner, books);
         ui.startUI();
     }
 }
